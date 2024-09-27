a4dcb9659cb9881c8b230d4824a8b75b9c391925
irevhub <refyandra@kts.co.id>
2024-09-27 19:53:39 +0700

    update

--- mt940-reader.py
+++ mt940-reader.py
@@ -3,6 +3,8 @@ import re
 import sys
 import hashlib
 import re
+
+
 # regEx
 # :61:240826DR176120660NTRF//0207150143062089
 # :61: 240826 DR 176120660 NTRF // 0207150143062089
@@ -13,6 +15,7 @@ import re
 # regex = r":61:(\d+)(\D{1,2})(\d+|\d+,\d{1,2})([A-Z]{4})(.{0,})"
 regex = r"(\d+)(\D{1,2})(\d+|\d+,\d{1,2})([A-Z]{4})(.{0,})"
 def parse_mt940_files(directory):
+    # os.system('clear')  
     if directory != None :
         transactions = {'case_in': 0, 'case_out': 0}
         # Loop melalui semua file di direktori yang ditentukan
@@ -20,12 +23,11 @@ def parse_mt940_files(directory):
             # if filename.endswith('.txt'):  # Sesuaikan ekstensi jika perlu
             if filename != None :  # Sesuaikan ekstensi jika perlu
                 # print(filename) #tampil file Name
-                file_path = os.path.join(directory, filename)
-                print(file_path)
+                file_path = os.path.join(directory, filename) 
                 with open(file_path, 'r') as file:
                     content = file.read()
                     # print(content)
-                    #print(file.name)
+                    # print(file.name)
                     # hash_file_md(file.name)
                 for entry in content.split(':61:')[1:]:  # regex = r"(\d+)(\D{1,2})(\d+|\d+,\d{1,2})([A-Z]{4})(.{0,})"
                     lines = entry.splitlines()
@@ -33,26 +35,31 @@ def parse_mt940_files(directory):
                     # print(f"entry: {(lines)}") 
                     matches = re.search(regex, lines[0])
                     if matches:    
-                        print(f"matches :61: {matches[1]} status:{matches[2]} amount:{matches[3]} code:{matches[4]}")
+                        print(f"matches :61: tgl:{matches[1]} status:{matches[2]} amount:{matches[3]} code:{matches[4]} file:{file.name}")
                         amount = float(matches[3].replace(',', '.'))
-                        if matches[2] == 'C'  or matches[2] == 'CR':  # CREDIT  ( BNI use  CR ) 
+                        if matches[2] == 'C' or matches[2] == 'CR':   # CREDIT
                             transactions['case_in'] += amount
-                        elif matches[2] == 'D'  or matches[2] == 'CR':  # DEBIT ( BNI use CR )
+                        elif matches[2] == 'D' or matches[2] == 'DR':  # DEBIT
                             transactions['case_out'] += amount
+
     return transactions
 
-if sys.argv[1]:
-  print(sys.argv[1])
+# print(len(sys.argv))
+if len(sys.argv)>=2:
   if os.path.isdir(sys.argv[1]):
     directory_path = sys.argv[1] #'D:/MT940/MT940/INJOURNEY/BMRI/Agustus/'  # Ganti dengan direktori Anda
   else:
-    print(f"Does {sys.argv[1]} exist or a directory?")  
+    print(f"Does {sys.argv[1]} exist or a file?")  
     sys.exit()
 else:
-  directory_path = 'D:/MT940/MT940/INJOURNEY/BMRI/Agustus/'
-
+  print(f"The directory has not yet stipulated, please select the destination directory. \nexp: python .\mt940.py /path/")  
+  directory_path = '.\data'
+#   sys.exit()
 # print(directory_path)
 result = parse_mt940_files(directory_path)
-print(f"Total Case In: {result['case_in']}, Total Case Out: {result['case_out']}")
+print(f"\n============================================================================")
+print(f"\nTotal Case In: {result['case_in']} |  Total Case Out: {result['case_out']}\n")
+print(f"============================================================================")
 
-sys.exit()
+
+# sys.exit()
